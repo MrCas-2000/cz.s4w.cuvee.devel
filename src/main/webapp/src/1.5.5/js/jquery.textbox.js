beb880dd61afacdc3855410aa98b3c878578ff2c
jdytrych@m2000.cz <jdytrych@m2000.cz@DESKTOP-D6VBC2I>
2020-11-18 05:51:23 +0100

    202011160551
    Signed-off-by: jdytrych@m2000.cz <jdytrych@m2000.cz@DESKTOP-D6VBC2I>

--- src/main/webapp/src/1.5.5/js/jquery.textbox.js
+++ src/main/webapp/src/1.5.5/js/jquery.textbox.js
@@ -536,7 +536,26 @@
 		return $.extend({}, $.fn.validatebox.parseOptions(_69), $.parser.parseOptions(_69, ["prompt", "iconCls", "iconAlign", "buttonText", "buttonIcon", "buttonAlign", "label", "labelPosition", "labelAlign", { multiline: "boolean", iconWidth: "number", labelWidth: "number" }]), { value: (t.val() || undefined), type: (t.attr("type") ? t.attr("type") : undefined) });
 	};
 	$.fn.textbox.defaults = $.extend({}, $.fn.validatebox.defaults, {
-		doSize: true, width: "auto", height: "auto", cls: null, prompt: "", value: "", type: "text", multiline: false, icons: [], iconCls: null, iconAlign: "right", iconWidth: 26, buttonText: "", buttonIcon: null, buttonAlign: "right", label: null, labelWidth: "auto", labelPosition: "before", labelAlign: "left", inputEvents: {
+		doSize: true, 
+		width: "auto", 
+		height: "auto", 
+		cls: null, 
+		prompt: "", 
+		value: "", 
+		type: "text", 
+		multiline: false, 
+		icons: [], 
+		iconCls: null, 
+		iconAlign: "right", 
+		iconWidth: 26, 
+		buttonText: "", 
+		buttonIcon: null, 
+		buttonAlign: "right", 
+		label: null, 
+		labelWidth: "auto", 
+		labelPosition: "before", 
+		labelAlign: "left", 
+		inputEvents: {
 			blur: function(e) {
 				var t = $(e.data.target);
 				var _6a = t.textbox("options");
